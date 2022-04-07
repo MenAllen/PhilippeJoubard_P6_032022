8cfb74123500a8e120ebaa383cfebf8d37d4d1ca
Philippe Joubard <philippe.joubard@sfr.fr>
2022-04-07 11:17:29 +0200

    first commit - etapes 1&2

---
+++ README.md
@@ -0,0 +1,6 @@
+# Base de code du projet P6 - Parcours Front-end
+Créer un site accessible pour une plateforme de photographes
+
+## Démarrer le projet
+Rien à installer ici, il suffit d'ouvrir le fichier `index.html`.
+
---
+++ scripts/pages/index.js
@@ -0,0 +1,23 @@
+// récupère les données du fichier photographsers.json
+function getPhotographers() {
+	return fetch("../data/photographers.json")
+		.then(response => response.json())
+		.catch(err => { throw (`la requete API fetch a échoué: ${err}`); });
+}
+
+async function displayData(photographers) {
+	const photographersSection = document.querySelector(".photographer_section");
+
+	photographers.forEach((photographer) => {
+		const photographerModel = photographerFactory(photographer);
+		const userCardDOM = photographerModel.getUserCardDOM();
+		photographersSection.appendChild(userCardDOM);
+	});
+}
+
+async function init() {
+	const { photographers } = await getPhotographers();
+	displayData(photographers);
+}
+
+init();
